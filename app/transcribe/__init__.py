fcbd7bcdb0e6c1e50899c338f1547c1b8f7d8bc2
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-03 18:00:39 +0300

    updated init

--- app/transcribe/__init__.py
+++ app/transcribe/__init__.py
@@ -1,14 +1,14 @@
-from flask import Blueprint
-from telegram import Bot
-import os
+# from flask import Blueprint
+# from telegram import Bot
+# import os
 
-from dotenv import load_dotenv
-load_dotenv(override=True)
+# from dotenv import load_dotenv
+# load_dotenv(override=True)
 
-api_key = os.getenv('TELEGRAM_API_KEY')
+# api_key = os.getenv('TELEGRAM_API_KEY')
 
-bot = Bot(api_key)
-transcribe = Blueprint('transcribe', __name__, template_folder='templates')
+# bot = Bot(api_key)
+# transcribe = Blueprint('transcribe', __name__, template_folder='templates')
 
 
-from app.transcribe import routes
+# from app.transcribe import routes
