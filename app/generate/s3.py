ca29422b1e154c906f0fb0733797c391052ffadb
Mike de Geofroy <mikedegeofroy@gmail.com>
2025-03-18 18:50:44 +0300

    Update s3.py

--- app/generate/s3.py
+++ app/generate/s3.py
@@ -8,7 +8,12 @@ aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 def upload(image_data, file_extension):
-  s3_client = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+  s3_client = boto3.resource(
+      's3',
+      endpoint_url='https://storage.yandexcloud.net',
+      aws_access_key_id,
+      aws_secret_access_key
+  )
   
   key = f'users/{str(uuid.uuid4())}{file_extension}'
   s3_client.Object(s3_bucket, key).put(Body=image_data)
